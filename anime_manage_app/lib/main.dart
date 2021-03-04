ec572cf2264260b43d093609858a7fed6adbd245
Ryo Wakabayashi <evangelionryo@gmail.com>
2021-03-05 06:02:26 +0900

    refactated example code by ChangeNotifier

--- anime_manage_app/lib/main.dart
+++ anime_manage_app/lib/main.dart
@@ -1,112 +1,108 @@
 import 'package:flutter/material.dart';
-import 'dart:developer';
-import 'history.dart';
-import 'favorites.dart';
-import 'weekly_schedule.dart';
-import 'setting.dart';
-import 'database.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter())
+      ],
+      child: new MyApp())
+);
 
 class MyApp extends StatelessWidget {
-  final Map<String, Widget> _pagesList = {
-    'schedule': Schedule(),
-    'favorites': Favorites(),
-    'history': ViewdHistory(),
-    'Setting': Setting()
-  };
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Anime Manager(Beta)',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        debugShowCheckedModeBanner: true,
-        home: MyHomePage(pages: _pagesList));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.pages}) : super(key: key);
-
-  final Map<String, Widget> pages;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Widget _showPage;
-  List<String> _pageKeyList = [];
-
-  @override
-  void initState() {
-    test();
-    // TODO: implement initState
-    _showPage = widget.pages['schedule'];
-    for (var key in widget.pages.keys) _pageKeyList.add(key);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Anime Manager(Beta)'),
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+      home: new MyHomePage('Flutter Hello World'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.lightGreen,
-              ),
+            new Text(
+              'You have pushed the button this many times:',
             ),
-            Container(
-                height: double.maxFinite,
-                // height: MediaQuery.of(context).size.height * 0.25,
-                padding: EdgeInsets.all(5),
-                child: ListView.builder(
-                    itemCount: _pageKeyList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text('${_pageKeyList[index]}'),
-                        onTap: () {
-                          _pageListTapped('${_pageKeyList[index]}');
-                        },
-                      );
-                    })),
+            const Count()
           ],
         ),
       ),
-      body: _showPage,
-
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
-      //     BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
-      //     BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
-      //   ],
-      //   currentIndex: _selectedIndex,
-      //   selectedItemColor: Colors.amber,
-      //   onTap: _onItemTapped,
-      // ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.arrow_right),
-      // ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () => context.read<Counter>().increment(),
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
 
-  void _pageListTapped(String index) {
-    setState(() {
-      Navigator.pop(context);
-      _showPage = widget.pages[index];
-    });
+class Counter with ChangeNotifier{
+  int _count = 0;
+  int get count => _count;
+
+  void increment(){
+    _count++;
+    notifyListeners();
   }
-} /**/
+}
+
+class Count extends StatelessWidget {
+  const Count({Key key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context){
+    return Text(
+        '${context.watch<Counter>().count}',
+         style: Theme.of(context).textTheme.headline4
+    );
+  }
+}
