4b6de58a41d2073e8028cdab077538ec13be053e
Ryo Wakabayashi <evangelionryo@gmail.com>
2021-03-18 00:30:56 +0900

    fix main.dart

--- anime_manage_app/lib/main.dart
+++ anime_manage_app/lib/main.dart
@@ -13,55 +13,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: true,
       home: WatchList(),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+// home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({Key key,this.title}):super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCouter(){
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'sefesrreesserse'
-          ),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headline2,
-          )
-        ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCouter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
--- anime_manage_app/lib/screen/WatchList.dart
+++ anime_manage_app/lib/screen/WatchList.dart
@@ -3,19 +3,6 @@ import 'package:flutter/material.dart';
 class WatchList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Hoge',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        debugShowCheckedModeBanner: true,
-        home: HomePage());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Watch List'),
@@ -32,28 +19,21 @@ Widget tileCard(BuildContext context,int _index) {
   _index++;
   return ListTile(
     leading: Icon(Icons.video_label),
-    // This icon will set video on-demand Icon (netflix youtube amapra etc...)
+// This icon will set video on-demand Icon (netflix youtube amapra etc...)
     title: Text('Story of Monster by Nishio Ishin'),
-    //This Title of Story
+//This Title of Story
     subtitle: Text('9 March 2021'),
-    //The Date that this title added at list
-    trailing: Text('EP$_index   '),
-    //Show Episode Number
+//The Date that this title added at list
+    trailing: Text('EP$_index '),
+//Show Episode Number
     onTap: () {
       confirmingDialog(context);
     }, //decoy function
-    /*
-  TODO
-  - automatic update list when wakeup app,from weekday schedule
-  - swipe tile to left, mean successful end to watch movie
-  - after it, show pop up for confirming
-  - tapped tile, move app 2 VOD app
-  - least title , show update symbol (ex. red circle)
-   */
+
   );
 }
 
-Future<Widget> confirmingDialog(BuildContext context) async {
+Future<dynamic> confirmingDialog(BuildContext context) async {
   return showDialog(
       context: context,
       builder: (_) {
