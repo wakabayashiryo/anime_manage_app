97d1c68333b90431e24e5a30cb56964c9778c772
Ryo Wakabayashi <evangelionryo@gmail.com>
2021-05-18 22:36:39 +0900

    implemeted CRUD funcs for SQlite

--- anime_manage_app/lib/database.dart
+++ anime_manage_app/lib/database.dart
@@ -1,6 +1,8 @@
+import 'package:anime_manage_app/database_helper.dart';
 import 'package:flutter/material.dart';
 
 class ProgramInformation {
+  int? id;
   String? title;
   int? totalEpisode;
   int? weekDays;
@@ -8,11 +10,12 @@ class ProgramInformation {
 
   @override
   String toString() {
-    return '{ ${this.title}, ${this.weekDays}, ${this.totalEpisode}, ${this.progress} }';
+    return '{ ${this.id} ${this.title}, ${this.weekDays}, ${this.totalEpisode}, ${this.progress} }';
   }
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'title': title,
       'totalEpisode': totalEpisode,
       'weekDays': weekDays,
@@ -20,229 +23,272 @@ class ProgramInformation {
     };
   }
 
+  /*
+    "[true,false,true,false....]" <= String type
+     to
+    [true,false,true,false....] <= List type
+   */
+  List<bool>? fromBoolString2List(String? text) {
+    List<bool> result = [];
+    final String hoge = text!.substring(1, text.length - 1);
+    final List<String> hoge2 = hoge.split(",");
+
+    hoge2.forEach((element) {
+      element == 'true' ? result.add(true) : result.add(false);
+    });
+
+    return result;
+  }
+
+  // Extract a Note object from a Map object
+  ProgramInformation.fromMapObject(Map<String, dynamic> map) {
+    this.id = map['id'];
+    this.title = map['title'];
+    this.totalEpisode = map['totalEpisode'];
+    this.weekDays = map['weekDays'];
+    this.progress = fromBoolString2List(map['progress']);
+  }
+
   ProgramInformation(
-      {this.title, this.weekDays, this.totalEpisode, this.progress});
+      {this.id, this.title, this.weekDays, this.totalEpisode, this.progress});
 }
 
 class ProgramInfoModel with ChangeNotifier {
-  List<ProgramInformation> _seedData = [];
+  List<ProgramInformation> _listData = [];
+  final dbHelper = DatabaseHelper.helperInstance;
 
   //initialize seedData
   ProgramInfoModel() {
-    //Sunday
-    _seedData.add(ProgramInformation(
-        title: '文豪ストレイドッグス　わん！',
-        weekDays: DateTime.sunday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'けいおん！！',
-        weekDays: DateTime.sunday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'OBSOLETE',
-        weekDays: DateTime.sunday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'のんのんびより　のんすとっぷ',
-        weekDays: DateTime.sunday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'おそ松さん',
-        weekDays: DateTime.sunday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
-    //Monday
-    _seedData.add(ProgramInformation(
-        title: '進撃の巨人 The Final Season',
-        weekDays: DateTime.monday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '7SEED',
-        weekDays: DateTime.monday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'SHIROBAKO',
-        weekDays: DateTime.monday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'シュタインズゲート',
-        weekDays: DateTime.monday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ウマ娘　プリティーダービー　Season 2',
-        weekDays: DateTime.monday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
-    //tuesday
-    _seedData.add(ProgramInformation(
-        title: '裏世界ピクニック',
-        weekDays: DateTime.tuesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'はたらく細胞!!',
-        weekDays: DateTime.tuesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'はたらく細胞BLACK',
-        weekDays: DateTime.tuesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ワールドウィッチーズ発進しますっ!',
-        weekDays: DateTime.tuesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ワンダーエッグ・プライオリティ',
-        weekDays: DateTime.tuesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'Re:ゼロから始めるい世界生活 第2期',
-        weekDays: DateTime.tuesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
-    //Wednesday
-    _seedData.add(ProgramInformation(
-        title: '七つの大罪憤怒の審判',
-        weekDays: DateTime.wednesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ゆるキャン△ SEASON2',
-        weekDays: DateTime.wednesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '約束のネバーランド Season2',
-        weekDays: DateTime.wednesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '弱キャラ友崎くん',
-        weekDays: DateTime.wednesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'アズールレーン　びそくぜんしんっ!',
-        weekDays: DateTime.wednesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '転生したらスライムだった件　第２期　第１部',
-        weekDays: DateTime.wednesday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
-    //Thursday
-    _seedData.add(ProgramInformation(
-        title: 'BEASTARS 第２期',
-        weekDays: DateTime.thursday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ホリミヤ',
-        weekDays: DateTime.thursday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '宇宙よりも遠い場所',
-        weekDays: DateTime.thursday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '八十亀ちゃんかんさつにっき　３さつめ',
-        weekDays: DateTime.thursday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
-    //Friday
-    _seedData.add(ProgramInformation(
-        title: '五等分の花嫁ff',
-        weekDays: DateTime.friday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '天地創造デザイン部',
-        weekDays: DateTime.friday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ゲキドル',
-        weekDays: DateTime.friday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ガンゲイル・オンライン',
-        weekDays: DateTime.friday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'ガウリール・ドロップアウト',
-        weekDays: DateTime.friday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
-    //Saturday
-    _seedData.add(ProgramInformation(
-        title: '無職転生　〜異世界行ったら本気だす〜',
-        weekDays: DateTime.saturday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: 'きんいろもざいく',
-        weekDays: DateTime.saturday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '呪術廻戦',
-        weekDays: DateTime.saturday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '<物語>シリーズ　セカンドシーズン',
-        weekDays: DateTime.saturday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-    _seedData.add(ProgramInformation(
-        title: '蜘蛛ですが、なにか?',
-        weekDays: DateTime.saturday,
-        totalEpisode: 12,
-        progress: List<bool>.filled(12, false)));
-
+    refreshData();
   }
 
-  List<ProgramInformation> get seedData => _seedData;
+  List<ProgramInformation> get listData => _listData;
 
-  void addTitle(ProgramInformation data) {
-    _seedData.add(data);
+  void refreshData() async {
+    _listData = await dbHelper.getDBlist();
+  }
+
+  void createTitle(ProgramInformation data) {
+    // _listData.add(data);
+    dbHelper.insert(data);
+    refreshData();
     notifyListeners();
   }
 
   void deleteTitle(int index) {
-    _seedData.remove(index);
+    // _listData.remove(index);
+    dbHelper.delete(index);
+    refreshData();
+    notifyListeners();
+  }
+
+  void updateTitle(ProgramInformation data) {
+    dbHelper.update(data);
+    refreshData();
     notifyListeners();
   }
 
   void toggleProgress(ProgramInformation data, int progindex) {
     data.progress?[progindex] = !data.progress![progindex];
+    refreshData();
     notifyListeners();
   }
 
   List<ProgramInformation>? searchByWeekday(int? weekday) {
-    return _seedData.where((element) => element.weekDays == weekday).toList();
+    return _listData.where((element) => element.weekDays == weekday).toList();
   }
 }
+
+// //Sunday
+// _listData.add(ProgramInformation(
+//     title: '文豪ストレイドッグス　わん！',
+//     weekDays: DateTime.sunday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'けいおん！！',
+//     weekDays: DateTime.sunday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'OBSOLETE',
+//     weekDays: DateTime.sunday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'のんのんびより　のんすとっぷ',
+//     weekDays: DateTime.sunday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'おそ松さん',
+//     weekDays: DateTime.sunday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+
+// //Monday
+// _listData.add(ProgramInformation(
+//     title: '進撃の巨人 The Final Season',
+//     weekDays: DateTime.monday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '7SEED',
+//     weekDays: DateTime.monday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'SHIROBAKO',
+//     weekDays: DateTime.monday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'シュタインズゲート',
+//     weekDays: DateTime.monday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ウマ娘　プリティーダービー　Season 2',
+//     weekDays: DateTime.monday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+
+// //tuesday
+// _listData.add(ProgramInformation(
+//     title: '裏世界ピクニック',
+//     weekDays: DateTime.tuesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'はたらく細胞!!',
+//     weekDays: DateTime.tuesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'はたらく細胞BLACK',
+//     weekDays: DateTime.tuesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ワールドウィッチーズ発進しますっ!',
+//     weekDays: DateTime.tuesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ワンダーエッグ・プライオリティ',
+//     weekDays: DateTime.tuesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'Re:ゼロから始めるい世界生活 第2期',
+//     weekDays: DateTime.tuesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+
+// //Wednesday
+// _listData.add(ProgramInformation(
+//     title: '七つの大罪憤怒の審判',
+//     weekDays: DateTime.wednesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ゆるキャン△ SEASON2',
+//     weekDays: DateTime.wednesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '約束のネバーランド Season2',
+//     weekDays: DateTime.wednesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '弱キャラ友崎くん',
+//     weekDays: DateTime.wednesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'アズールレーン　びそくぜんしんっ!',
+//     weekDays: DateTime.wednesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '転生したらスライムだった件　第２期　第１部',
+//     weekDays: DateTime.wednesday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+
+// //Thursday
+// _listData.add(ProgramInformation(
+//     title: 'BEASTARS 第２期',
+//     weekDays: DateTime.thursday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ホリミヤ',
+//     weekDays: DateTime.thursday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '宇宙よりも遠い場所',
+//     weekDays: DateTime.thursday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '八十亀ちゃんかんさつにっき　３さつめ',
+//     weekDays: DateTime.thursday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+
+// //Friday
+// _listData.add(ProgramInformation(
+//     title: '五等分の花嫁ff',
+//     weekDays: DateTime.friday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '天地創造デザイン部',
+//     weekDays: DateTime.friday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ゲキドル',
+//     weekDays: DateTime.friday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ガンゲイル・オンライン',
+//     weekDays: DateTime.friday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'ガウリール・ドロップアウト',
+//     weekDays: DateTime.friday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+
+// //Saturday
+// _listData.add(ProgramInformation(
+//     title: '無職転生　〜異世界行ったら本気だす〜',
+//     weekDays: DateTime.saturday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: 'きんいろもざいく',
+//     weekDays: DateTime.saturday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '呪術廻戦',
+//     weekDays: DateTime.saturday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '<物語>シリーズ　セカンドシーズン',
+//     weekDays: DateTime.saturday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
+// _listData.add(ProgramInformation(
+//     title: '蜘蛛ですが、なにか?',
+//     weekDays: DateTime.saturday,
+//     totalEpisode: 12,
+//     progress: List<bool>.filled(12, false)));
--- anime_manage_app/lib/database_helper.dart
+++ anime_manage_app/lib/database_helper.dart
@@ -5,12 +5,85 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'database.dart';
 
-//TODO
-// sqfliteの振る舞いを確認
-// ProgramInformationと統合させる
-//
+class DatabaseHelper {
+  final String tableName = "titleData";
+  int? version = 1;
+  final String tableStructure = """
+  id INTEGER PRIMARY KEY, 
+  title TEXT, 
+  totalEpisode INTEGER,
+  weekDays INTEGER,
+  progress TEXT,
+  """;
+
+  // make this a singleton class
+  DatabaseHelper._createInstance();
+  static final DatabaseHelper helperInstance = DatabaseHelper._createInstance();
+
+  static Database? _database;
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+
+    _database = await _initDatabase();
+
+    return database;
+  }
+
+  _initDatabase() async {
+    Directory activeDirectory = await getApplicationDocumentsDirectory();
+    String path = join(activeDirectory.path, tableName + ".db");
+    return await openDatabase(path, version: version, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE 
+          $tableName
+          ($tableStructure)
+          ''');
+  }
+
+  Future<int?> insert(ProgramInformation info) async {
+    Database? db = await this.database;
+    return await db!.insert(tableName, info.toMap());
+  }
+
+  Future<int?> update(ProgramInformation info) async {
+    Database? db = await this.database;
+    return await db!.update(tableName, info.toMap(),where: 'id = ?',whereArgs: [info.id]);
+  }
+
+  Future<int?> delete(int id) async {
+    Database? db = await this.database;
+    return await db!.delete(tableName, where: 'id = ?',whereArgs: [id]);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    Database? db = await this.database;
+    return await db!.query(tableName);
+  }
+
+  //get ths 'Map List' [List<Map>] and convert it to 'ProgramInformation List' [List<ProgramInformation>]
+  Future<List<ProgramInformation>> getDBlist() async {
+    var mapList = await queryAllRows();
+    int listCount = mapList.length;
+    List<ProgramInformation> infoList = [];
+
+    for (int index = 0; index < listCount; listCount++)
+      infoList.add(ProgramInformation.fromMapObject(mapList[index]));
+
+    return infoList;
+  }
+
+  Future<int?> queryRowCount() async {
+    Database? db = await helperInstance.database;
+    return Sqflite.firstIntValue(
+        await db!.rawQuery('SELECT COUNT(*) FROM $tableName'));
+  }
+}
+
 void test() async {
-  
   final databaseName = 'hoge.db';
   final databasePath = await getDatabasesPath();
 
@@ -31,77 +104,4 @@ void test() async {
   final List<Map<String, dynamic>> gotName = await database.query('name');
   print(gotName[0]);
   print(databasePath);
-
-}
-
-class DatabaseHelper {
-  final String tableName = "titleData.db";
-  int? version = 1;
-  final String tableStructure = 
-  """
-  CREATE TABLE 
-  titleData(
-    id INTEGER PRIMARY KEY, 
-    title TEXT, 
-    totalEpisode INTEGER,
-    weekDays INTEGER,
-    progress TEXT,
-    )
-  """;
-
- // make this a singleton class
-  DatabaseHelper._createInstance();
-  static final DatabaseHelper helperInstance = DatabaseHelper._createInstance();
-
-  static Database? _database;
-
-  Future<Database?> get database async {
-    if (_database != null) return _database;
-
-    _database = await _initDatabase();
-
-    return database;
-  }
-
-  _initDatabase() async {
-    Directory activeDirectory = await getApplicationDocumentsDirectory();
-    String path = join(activeDirectory.path, tableName);
-    return await openDatabase(path, version: version, onCreate: _onCreate);
-  }
-
-  List<bool>? fromBoolString2List(String? text) {
-    List<bool> result = [];
-    final String hoge = text!.substring(1, text.length - 1);
-    final List<String> hoge2 = hoge.split(",");
-
-    hoge2.forEach((element) {
-      element == 'true' ? result.add(true) : result.add(false);
-    });
-    
-    return result;
-  }
-
-  Future _onCreate(Database db, int version) async {
-    await db.execute('''
-          CREATE TABLE 
-          ${this.tableName}
-          (${this.tableStructure})
-          ''');
-  }
-
-  Future<int?> insert(Map<String, dynamic> row) async {
-    Database? db = await helperInstance.database;
-    return await db!.insert(tableName, row);
-  }
-
-  Future<List<Map<String, dynamic>>> queryAllRows() async {
-    Database? db = await helperInstance.database;
-    return await db!.query(tableName);
-  }
-
-  Future<int?> queryRowCount() async {
-    Database? db = await helperInstance.database;
-    return Sqflite.firstIntValue(
-        await db!.rawQuery('SELECT COUNT(*) FROM $tableName'));
-  }
 }
